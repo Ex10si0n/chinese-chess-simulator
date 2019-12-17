17268c4111a2e61247b6bf8e65234e53ec4bcbad
Ex10si0n <yzb.ex10si0n@icloud.com>
2019-12-17 16:12:24 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -16,8 +17,10 @@ public class Main {
         for(int i = 1; i <= 10; i++) for(int j = 1; j <= 9; j++) map[i][j]='口';
         map[1][1] = map[10][1] = map[1][9] = map [10][9] = '车';
         map[1][2] = map[10][2] = map[1][8] = map [10][8] = '马';
-        map[1][3] = map[10][3] = map[1][7] = map [10][7] = '象';
-        map[1][4] = map[10][4] = map[1][6] = map [10][6] = '士';
+        map[1][3] = map[1][7] = '象';
+        map[10][3] = map [10][7] = '相';
+        map[1][4] = map[1][6] = '士';
+        map[10][4] = map [10][6] = '仕';
         map[1][5] = '帅'; map[10][5] = '将';
         map[3][2] = map[8][2] = map[3][8] = map[8][8] =  '炮';
         map[4][1] = map[4][3] = map[4][5] = map[4][7] = map[4][9] = '卒';
@@ -49,6 +52,7 @@ public class Main {
 
         //打印势力
         if(false){
+            System.out.println();
             for(int i = 1; i <= 10; i++) {
                 for(int j = 1; j <= 9; j++) {
                     System.out.print(isPlayerUp[i][j] + "  ");
@@ -56,8 +60,6 @@ public class Main {
                 System.out.println();
             }
         }
-
-
     }
 
     public static void changeRoll() {
@@ -66,7 +68,8 @@ public class Main {
         playerNow = !playerNow;
     }
 
-    public static void run() {
+    public static void run() throws IOException {
+
         String cnNum = "零一二三四五六七八九";
         String arNum = "0123456789";
         Scanner scanner = new Scanner(System.in);
@@ -116,35 +119,35 @@ public class Main {
                                 map[i - 2][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i - 2][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else if(Math.abs(start - end) == 2) {
                                 map[i - 1][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i - 1][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else {
                                 printError();
                             }
                         }
-                        if(main == '象') {
+                        if(main == '象'|| main == '相') {
                             if(Math.abs(start - end) == 2) {
                                 map[i - 2][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i - 2][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else {
                                 printError();
                             }
                         }
-                        if(main == '士') {
+                        if(main == '士'|| main == '仕') {
                             if(Math.abs(start - end) == 1) {
                                 map[i - 1][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i - 1][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else {
                                 printError();
@@ -153,7 +156,7 @@ public class Main {
                         map[i - end][10 - start] = map[i][10 - start];
                         map[i][10 - start] = '口';
                         isPlayerUp[i - end][10 - start] = isPlayerUp[i][10 - start];
-                        isPlayerUp[i][10 - start] = true;
+                        isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                         break;
                     }
                 }
@@ -165,35 +168,35 @@ public class Main {
                                 map[i + 2][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i + 2][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else if(Math.abs(start - end) == 2) {
                                 map[i + 1][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i + 1][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else {
                                 printError();
                             }
                         }
-                        if(main == '象') {
+                        if(main == '象' || main == '相') {
                             if(Math.abs(start - end) == 2) {
                                 map[i + 2][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i + 2][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else {
                                 printError();
                             }
                         }
-                        if(main == '士') {
+                        if(main == '士' || main == '仕') {
                             if(Math.abs(start - end) == 1) {
                                 map[i + 1][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i + 1][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else {
                                 printError();
@@ -201,8 +204,8 @@ public class Main {
                         }
                         map[i + end][start] = map[i][start];
                         map[i][start] = '口';
-                        isPlayerUp[i][start] = isPlayerUp[i][start];
-                        isPlayerUp[i + end][start] = !isPlayerUp[i][start];
+                        isPlayerUp[i + end][start] = isPlayerUp[i][start];
+                        isPlayerUp[i][start] = !isPlayerUp[i][start];
                         break;
                     }
                 }
@@ -217,36 +220,36 @@ public class Main {
                                 map[i + 2][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i + 2][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else if(Math.abs(start - end) == 2) {
                                 map[i + 1][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i + 1][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else {
                                 printError();
                             }
 
                         }
-                        if(main == '象') {
+                        if(main == '象' || main == '相') {
                             if(Math.abs(start - end) == 2) {
                                 map[i + 2][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i + 2][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else {
                                 printError();
                             }
                         }
-                        if(main == '士') {
+                        if(main == '士'|| main == '仕') {
                             if(Math.abs(start - end) == 1) {
                                 map[i + 1][10 - end] = map[i][10 - start];
                                 map[i][10 - start] = '口';
                                 isPlayerUp[i + 1][10 - end] = isPlayerUp[i][10 - start];
-                                isPlayerUp[i][10 - start] = true;
+                                isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                                 break;
                             }else {
                                 printError();
@@ -255,7 +258,7 @@ public class Main {
                         map[i + end][10 - start] = map[i][10 - start];
                         map[i][10 - start] = '口';
                         isPlayerUp[i + end][10 - start] = isPlayerUp[i][10 - start];
-                        isPlayerUp[i][10 - start] = true;
+                        isPlayerUp[i][10 - start] = !isPlayerUp[i][10 - start];
                         break;
                     }
                 }
@@ -267,36 +270,36 @@ public class Main {
                                 map[i - 2][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i - 2][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else if(Math.abs(start - end) == 2) {
                                 map[i - 1][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i - 1][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else {
                                 printError();
                             }
 
                         }
-                        if(main == '象') {
+                        if(main == '象' || main == '相') {
                             if(Math.abs(start - end) == 2) {
                                 map[i - 2][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i - 2][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else {
                                 printError();
                             }
                         }
-                        if(main == '士') {
+                        if(main == '士'|| main == '仕') {
                             if(Math.abs(start - end) == 1) {
                                 map[i - 1][end] = map[i][start];
                                 map[i][start] = '口';
                                 isPlayerUp[i - 1][end] = isPlayerUp[i][start];
-                                isPlayerUp[i][start] = true;
+                                isPlayerUp[i][start] = !isPlayerUp[i][start];
                                 break;
                             }else {
                                 printError();
@@ -316,7 +319,7 @@ public class Main {
     public static void debug() {
        // endGame = true;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         init();
         while(!endGame) {
             printMap();
